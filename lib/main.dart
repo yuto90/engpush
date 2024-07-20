cef3280a9697bdee134908d4bb47fd08fc88b2c1
yuto90 <solidusgako4087@gmail.com>
2024-07-20 22:18:51 +0900

    add: プッシュ通知(Android)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,50 @@
+import 'package:engpush/firebase_options.dart';
 import 'package:engpush/view/example.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+// バックグラウンドメッセージハンドラ
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message: ${message.messageId}');
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Firebaseの初期化
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // バックグラウンドメッセージハンドラの設定
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  // 通知権限のリクエスト
+  await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  // FCMトークンの取得
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print('FCM Token: $fcmToken');
+
+  // フォアグラウンドでのメッセージハンドラ
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Received message: ${message.notification?.title}');
+    // フォアグラウンドでの通知処理
+  });
+
+  // アプリが開かれた時のメッセージハンドラ
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+    print('Message opened app: ${message.notification?.title}');
+    // バックグラウンドでアプリを開いた時の処理
+  });
   runApp(const ProviderScope(child: MyApp()));
 }
 
